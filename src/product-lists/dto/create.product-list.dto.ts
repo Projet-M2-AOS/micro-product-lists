b36bf14e015ba72bbb87e5d538383be83d3fe498
TwioZz <yoann.lebretonpro@gmail.com>
2021-12-02 18:59:29 +0100

    add dto

--- src/product-lists/dto/create.product-list.dto.ts
+++ src/product-lists/dto/create.product-list.dto.ts
@@ -1,3 +1,22 @@
+import mongoose from "mongoose";
+import {ArrayMaxSize, IsArray, IsNotEmpty, IsString, Length} from "class-validator";
+
 export class CreateProductListDto {
 
+    @IsNotEmpty()
+    @IsString()
+    @Length(0, 100)
+    user: mongoose.Schema.Types.ObjectId
+
+    @IsNotEmpty()
+    @IsString()
+    @Length(0, 100)
+    name: string
+
+    @IsNotEmpty()
+    @IsArray()
+    @ArrayMaxSize(15)
+    @IsString({each: true})
+    @Length(0, 100, { each: true })
+    products: mongoose.Schema.Types.ObjectId[]
 }
--- src/product-lists/dto/update.product-list.dto.ts
+++ src/product-lists/dto/update.product-list.dto.ts
@@ -1,3 +1,21 @@
-export class UpdateProductListDto {
+import {ArrayMaxSize, IsArray, IsOptional, IsString, Length} from "class-validator";
+import mongoose from "mongoose";
 
+export class UpdateProductListDto {
+    @IsOptional()
+    @IsString()
+    @Length(0, 100)
+    user: mongoose.Schema.Types.ObjectId
+
+    @IsOptional()
+    @IsString()
+    @Length(0, 100)
+    name: string
+
+    @IsOptional()
+    @IsArray()
+    @ArrayMaxSize(15)
+    @IsString({each: true})
+    @Length(0, 100, { each: true })
+    products: mongoose.Schema.Types.ObjectId[]
 }
