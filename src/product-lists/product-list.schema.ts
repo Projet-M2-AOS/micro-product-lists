4dcda57a234ef579821ed8e97cb3076e056e949e
TwioZz <yoann.lebretonpro@gmail.com>
2021-12-28 16:31:06 +0100

    forget @Schema

--- src/product-lists/product-list.schema.ts
+++ src/product-lists/product-list.schema.ts
@@ -1,10 +1,11 @@
 import * as mongoose from "mongoose";
-import {Prop, SchemaFactory} from "@nestjs/mongoose";
+import {Prop, Schema, SchemaFactory} from "@nestjs/mongoose";
 import {Document} from "mongoose";
 import {ApiProperty} from "@nestjs/swagger";
 
 export type ProductListDocument = ProductList & Document;
 
+@Schema()
 export class ProductList {
     @ApiProperty({
         type: String,
